a9a15108e509d77276f6b345e2e870e3aa4c436f
Wooseung Sim <Wooseung.Sim@soti.net>
2017-09-26 14:25:25 -0400

    test

---
+++ Test-S3ObjectCopy.ps1
@@ -0,0 +1,92 @@
+﻿    [CmdletBinding(PositionalBinding = $false)]
+    param 
+    (
+        [Parameter(mandatory=$false)]
+        [switch] $Upload,
+
+        [Parameter(mandatory=$false)]
+        [string] $PipelineCounter = $env:GO_PIPELINE_NAME,
+
+        [Parameter(mandatory=$false)]
+        [string] $PipelineName = $env:GO_PIPELINE_COUNTER,
+
+        [Parameter(mandatory=$false)]
+        [string] $AccessKey = $env:ACCESS_KEY,
+
+        [Parameter(mandatory=$false)]
+        [string] $SecretKey = $env:SECRET_KEY,
+
+        [Parameter(mandatory=$false)]
+        [string] $Region = 'us-east-1'
+    )
+BEGIN
+{
+    Set-DefaultAWSRegion -Region $Region | Out-Null
+    Set-AWSCredential -AccessKey $AccessKey -SecretKey $SecretKey | Out-Null   
+    [string] $bucket = 'mobicontrol-uat-installers'
+}
+PROCESS
+{
+    $s3Key = "test_$PipelineName_$PipelineCounter"
+    $currentPath = Resolve-Path '.\'
+    $workDir = Join-Path $currentPath $s3Key
+
+    [int] $total = 0
+
+    [datetime] $start = [datetime]::MinValue
+
+    if($Upload)
+    {
+        $files = Get-ChildItem -Path $workDir
+        $sizes = $files | Select-Object -ExpandProperty Length
+        $total = [Linq.Enumerable]::Sum( [int[]]$sizes)
+
+        $objects = Get-S3Object -BucketName $bucket -KeyPrefix $s3Key
+
+        if ($objects -ne $null -and $objects.Count -gt 0)
+        {
+            foreach ($obj in $objects)
+            {
+                Remove-S3Object -BucketName $bucket -Key $obj.Key  -Force | Out-Null
+            } 
+        }
+
+        Write-Host "Start to uploading $($objects.Count) files ($total bytes)."
+        $start = [datetime]::Now
+        Write-S3Object -BucketName $bucket -KeyPrefix $s3Key -Folder $workDir -Recurse -Force | Out-Null
+    }
+    else
+    {
+        if (Test-Path -Path $workDir)
+        {
+            Remove-Item -Path $workDir -Force -Recurse
+        }
+
+        $objects = Get-S3Object -BucketName $bucket -KeyPrefix $s3Key
+        if($objects -eq $null -or $objects.Count -eq 0)
+        {
+            $s3Key = 'test'
+            $objects = Get-S3Object -BucketName $bucket -KeyPrefix $s3Key
+        }
+        $sizes = $objects | Select-Object -ExpandProperty Size
+        $total = [Linq.Enumerable]::Sum( [int[]]$sizes)
+
+        Write-Host "Start to download $($objects.Count) files ($total bytes)."
+        $start = [datetime]::Now
+
+        foreach ($obj in $objects)
+        {
+            Copy-S3Object -BucketName $bucket -Key $obj.Key  -Force -LocalFolder $workDir | Out-Null
+        }
+        Write-Host "Download completed"
+    }
+    [datetime] $end = [datetime]::Now
+
+    [timespan] $timeDiff = $end - $start
+    $bytePerSecond = $total / $timeDiff.TotalSeconds
+
+
+    Write-Host ""
+    Write-Host "Started at $start,  Ended at $end, Took $($timeDiff.TotalSeconds) seconds"
+    Write-Host "Byte per second: $bytePerSecond."
+}
